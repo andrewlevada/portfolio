b6a7f69410dc417e18939841df0877f44c1cc394
AndrewLevada <levada.andrew@gmail.com>
2024-09-09 14:35:47 +0300

    case card styles hotfix

--- src/components/cases-section.tsx
+++ src/components/cases-section.tsx
@@ -11,7 +11,7 @@ export default function CasesSection() {
             
             <div className="grid grid-cols-3 gap-x-[8px] gap-y-[40px] w:grid-cols-2 tiny:!grid-cols-1">
                 {cases.map((item, index) => (
-                    <div key={index} className="flex flex-col gap-[17px] min-h-full justify-between">
+                    <div key={index} className="flex flex-col gap-[17px] min-h-full justify-between tiny:justify-start tiny:h-fit tiny:min-h-fit">
                         <div className="flex flex-col gap-[3px] mr-[60px] tiny:mr-0">
                             <p className="opacity-40">{item.context}</p>
                             <p>{item.task} → {item.result}</p>
